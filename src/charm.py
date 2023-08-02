c02caad6241a400636b84d2a8a6407b1efab2a9c
Kaioh <gupajibril@gmail.com>
2023-08-02 13:48:23 +0100

    initial commit

---
+++ src/charm.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# Copyright 2023 Kaioh
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""Charm the service.
+
+Refer to the following tutorial that will help you
+develop a new k8s charm using the Operator Framework:
+
+https://juju.is/docs/sdk/create-a-minimal-kubernetes-charm
+"""
+
+import logging
+import os
+import shutil
+
+from ops.charm import CharmBase
+from ops.framework import StoredState
+from ops.main import main
+from ops.model import ActiveStatus, MaintenanceStatus
+
+# Log messages can be retrieved using juju debug-log
+logger = logging.getLogger(__name__)
+
+
+
+class PracticeCharmCharm(CharmBase):
+    _stored = StoredState()
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.start, self._on_start)
+        self.framework.observe(self.on.stop, self._on_stop)
+        #self._stored.set_default(message=self.config["message"])
+
+    def _on_install(self, event):
+        logger.info(f"Installing cmatrix")
+        os.system('apt install -y cmatrix')
+
+    # def _on_config_changed(self, event):
+    #     """ 
+    #     Update the message configuration and set the unit status
+    #     """
+
+    #     self.unit.status = ActiveStatus(f"")
+
+    def _on_start(self, event):
+        logger.info(f"Starting cmatrix")
+        os.system('cmatrix')
+        if not os.system('apt list | grep cmatrix') == 0:
+            logger.info("cmatrix is not installed")
+            self.unit.status = MaintenanceStatus("Not installed.")
+        else:
+            logger.info(f"cmatrix is running")
+            self.unit.status = ActiveStatus("Running")
+
+    def _on_stop(self, event):
+        logger.info(f"Stopping cmatrix")
+        shutil.copyfile('bash/cmatrix.sh', '~')
+        os.system('bash cmatrix.sh')
+
+if __name__ == "__main__": 
+    main(PracticeCharmCharm)
